dbe5a245f72560ed6b23a936a015d57f079ac01c
Anurag Pramanik <p.anurag@outlook.com>
2022-10-02 21:45:01 +0530

    Add todo logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './App.css'
 import InputField from './components/InputField'
 import { Todo } from './model'
+import TodoList from './components/TodoList'
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('')
@@ -12,16 +13,18 @@ const App: React.FC = () => {
 
     if (todo) {
       setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo('')
     }
   }
 
-  console.log(todo)
+  console.log(todos)
 
   return (
     <div className='App'>
       <span className='heading'>Taskify</span>
       {/* Try with handler function instead of passing state as props */}
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   )
 }
